661bf95a96e8a356c471a97cd9c4936a4e000d32
taikwando <tsavage@udel.edu>
2018-01-02 12:12:51 -0500

    added needs for twitter scraper

---
+++ WebScraper/twitScraper.py
@@ -0,0 +1,13 @@
+from urllib2 import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+# PART ONE: NUMBER OF CHARACTERS
+# Using Beautiful Soup, the script will parse through each tweet, grab the text and count the number of characters used,
+# and then display the information using a data visualization tool (matplotlib / Seaborn).
+
+# NEEDS:
+# map out how to get to tweet information on twitter []
+# read up how twitter allows you to use its data (check for another way to accomplish same goal) []
+# pull tweet data from twitter []
+# learn how data is used by matplotlib & Seaborn []
+
